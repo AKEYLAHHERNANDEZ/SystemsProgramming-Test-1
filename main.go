64b1572f7f3239f5eb4c27f73da5d055b541781a
AKEYLAHHERNANDEZ <119026429+AKEYLAHHERNANDEZ@users.noreply.github.com>
2025-04-03 01:44:26 +0000

    Logic fix & grammar

--- main.go
+++ main.go
@@ -32,77 +32,82 @@ var (
 
 func worker(wg *sync.WaitGroup, tasks chan string, result chan Definitions, dialer net.Dialer) {
 	defer wg.Done()
-    for addr := range tasks {
+	for addr := range tasks {
 		division := strings.Split(addr, ":")
-		host,Variable := division[0], division[1]
-		port,_ := strconv.Atoi(Variable)
-
-		conn, err := net.DialTimeout("tcp", addr,timeout)
+		if len(division) != 2 {
+			continue
+		}
+		host, Variable := division[0], division[1]
+		port, err := strconv.Atoi(Variable)
+		if err != nil {
+			continue
+		}
+		conn, err := net.DialTimeout("tcp", addr, timeout)
 		if err == nil {
-			conn.Close()
-			
-			const(
-			buffersize = 1024
-			timeout = 2 * time.Second
-			)
-
-			if check {
-				banner,err := grabbanner (conn,buffersize,timeout)
-				if err != nil {
-					banner = ""
-				}
-				result <- Definitions{
-					Host: host,
-					Port: port,
-					Check: true,
-					Service: service,
-					Protocol: protocol,
-				}
+			result <- Definitions{
+				Host:  host,
+				Port:  port,
+				Check: true,
 			}
-				else {
-					result <- Definitions {
-						Host: host,
-						Port: port,
-						Check: false,
-					}	
-			}
-
-			func GrabberHelper(conn net.Coon, buffersizee int, timeout time.Duration) (string, error) {
-			banner := make([byte,buffersize])
-			CONN.SetReadDeadline(time.Now().Add(timeout))
-			Vari, err :=conn.Read(bannerBuffer)
+			defer conn.Close()
+		}
+		if check {
+			banner, err := GrabberHelper(conn, 1024, 2*time.Second)
 			if err != nil {
-				return " ",err
+				banner = ""
 			}
-			return string(bannerBuffer[:vari],nil)
+			result <- Definitions{
+				Host:     host,
+				Port:     port,
+				Check:    true,
+				Service:  banner,
+				Protocol: protocol,
+			}
+		} else {
+			result <- Definitions{
+				Host:  host,
+				Port:  port,
+				Check: true,
+			}
+		}
+	} 
+} 
+func GrabberHelper(conn net.Conn, bufferSize int, timeout time.Duration) (string, error) {
+			banner := make([]byte,bufferSize)
+			conn.SetReadDeadline(time.Now().Add(timeout))
+			Vari, err := conn.Read(banner)
+			if err != nil {
+				return "",err
+			}
+			return string(banner[:Vari]), nil
 			}
 
 
-func main() {
+// func main() {
 
-	var wg sync.WaitGroup
-	tasks := make(chan string, 100)
+// 	var wg sync.WaitGroup
+// 	tasks := make(chan string, 100)
 
-    target := "scanme.nmap.org"
+//     target := "scanme.nmap.org"
 
-	dialer := net.Dialer {
-		Timeout: 5 * time.Second,
-	}
+// 	dialer := net.Dialer {
+// 		Timeout: 5 * time.Second,
+// 	}
   
-	workers := 100
+// 	workers := 100
 
-    for i := 1; i <= workers; i++ {
-		wg.Add(1)
-		go worker(&wg, tasks, dialer)
-	}
+//     for i := 1; i <= workers; i++ {
+// 		wg.Add(1)
+// 		go worker(&wg, tasks, dialer)
+// 	}
 
-	ports := 512
+// 	ports := 512
 
-	for p := 1; p <= ports; p++ {
-		port := strconv.Itoa(p)
-        address := net.JoinHostPort(target, port)
-		tasks <- address
-	}
-	close(tasks)
-	wg.Wait()
-}
+// 	for p := 1; p <= ports; p++ {
+// 		port := strconv.Itoa(p)
+//         address := net.JoinHostPort(target, port)
+// 		tasks <- address
+// 	}
+// 	close(tasks)
+// 	wg.Wait()
+// }
